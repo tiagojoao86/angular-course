607febe799b88df6edfcc724cfe6ea1ab170ecc9
tiagojoao86 <pereira.tiagojoao@gmail.com>
2018-07-29 11:44:12 -0300

    domingo 

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,15 +6,23 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // None, Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
 
   @Input('srvElement') element: {type: string,
   name:string,
   content: string};
 
-  constructor() { }
+  constructor() { 
+    console.log('constructor called.')
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges called.')
+    console.log(changes);
+  }
 
   ngOnInit() {
+    console.log('ngOnInit called.')
   }
 
 }
