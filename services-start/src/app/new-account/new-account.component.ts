890cf64d1b44a729c87051f6c6844d1a72860d0d
tiagojoao86 <pereira.tiagojoao@gmail.com>
2018-08-05 16:37:22 -0300

    services-start

---
+++ services-start/src/app/new-account/new-account.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
+})
+export class NewAccountComponent {
+  
+  constructor(private logginService: LoggingService,
+              private accountsService: AccountsService){}
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountsService.addAccount(accountName, accountStatus);
+    this.logginService.logStatusChange(accountStatus);
+  }
+}
