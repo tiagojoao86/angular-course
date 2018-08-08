812ccd80b4c368ff8ebcd2abb384fa8fafce917e
tiagojoao86 <pereira.tiagojoao@gmail.com>
2018-08-08 00:43:37 -0300

    service

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -6,7 +6,7 @@ import { AccountsService } from '../accounts.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  //providers: [LoggingService]
 })
 export class NewAccountComponent {
   
@@ -15,6 +15,6 @@ export class NewAccountComponent {
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.logginService.logStatusChange(accountStatus);
+    //this.logginService.logStatusChange(accountStatus);
   }
 }
