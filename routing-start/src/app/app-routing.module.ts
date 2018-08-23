bd3d09cd9fff7872a80220c99e067d9bcb49afbd
tiagojoao86 <pereira.tiagojoao@gmail.com>
2018-08-23 13:23:25 -0300

    static

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ import { EditServerComponent } from "./servers/edit-server/edit-server.component
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
+import { ServerResolver } from "./servers/server-resolver.service";
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -20,12 +22,13 @@ const appRoutes: Routes = [
     //canActivate: [AuthGuard],
     canActivateChild: [AuthGuard], 
     component: ServersComponent, children: [
-      { path: ':id', component: ServerComponent },
+      { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
       { path: ':id/edit', component: EditServerComponent, 
         canDeactivate: [CanDeactivateGuard] }
     ] },
     
-    { path: 'not-found', component: PageNotFoundComponent },
+    //{ path: 'not-found', component: PageNotFoundComponent },
+    { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found.'} },
     { path: '**', redirectTo: '/not-found' }
 ];
 
