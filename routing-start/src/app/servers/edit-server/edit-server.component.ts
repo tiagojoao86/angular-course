6758727686a74f465ef250eebff2811ae799091d
tiagojoao86 <pereira.tiagojoao@gmail.com>
2018-08-22 23:43:04 -0300

    deac

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
@@ -43,6 +45,20 @@ export class EditServerComponent implements OnInit {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
     this.changesSaved = true;
     this.router.navigate(['../'], { relativeTo: this.route });
+  }
+
+  canDeactivate():Observable<boolean> | Promise<boolean> | boolean{
+    if(!this.allowEdit){
+      return true;
+    }
+    if((this.serverName !== this.server.name 
+        || this.serverStatus !== this.server.status) &&
+          !this.changesSaved) {
+            return confirm('Do you want to discard changes?')
+    }
+    else{
+      return true;
+    }
   }
 
 }
