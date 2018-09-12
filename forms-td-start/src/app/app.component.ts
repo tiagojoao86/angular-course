7778abdc5d4b20c2d9b6ccd3975686158135e74a
tiagojoao86 <pereira.tiagojoao@gmail.com>
2018-09-12 00:29:04 -0300

    forms01

---
+++ forms-td-start/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit(form: NgForm) {
+    console.log(this.signupForm);
+  }
+
+}
