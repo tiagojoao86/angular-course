4a9516b78bd5d0885d73cf472b64b1a9d6d6c18a
tiagojoao86 <pereira.tiagojoao@gmail.com>
2018-09-28 00:57:01 -0300

    28092018

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,22 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
+export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  submitted: boolean = false;
   options = [
     {id: 0, name:"Basic"},
     {id: 1, name:"Advanced"},
     {id: 2, name:"Pro"}
   ];
+
+  user = {
+    email: '',
+    password: '',
+    type: ''
+  }
   
   defaultQuestion = this.options[1].name;
-
-  ngOnInit(){
-    
+  onSubmit() {
+    this.submitted = true;
+    this.user.email = this.signupForm.value.email;
+    this.user.password = this.signupForm.value.password;
+    this.user.type = this.signupForm.value.type;
+    this.signupForm.reset();
   }
-//"Basic", "Advanced", "Pro"
 }
